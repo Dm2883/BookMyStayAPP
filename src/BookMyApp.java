621f6d8dac9f7ccc71be9d0beacdc00f82054ad2
Dm2883 <devarajmurugan45@gmail.com>
2026-03-12 09:06:43 +0530

    UC1 - Application entry and welcome message

--- src/BookMyApp.java
+++ src/BookMyApp.java
@@ -1,2 +1,10 @@
 public class BookMyApp {
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to BookMyStay App");
+        System.out.println("=================================");
+        System.out.println("System started successfully.");
+
+    }
 }
